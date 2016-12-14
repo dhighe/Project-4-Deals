dafc7655d7984e22d88b48afde11be3fdf550df3
Donald Highe <dhighe25@gmail.com>
2016-12-14 16:26:38 -0500

    deploying

--- webpack.config.js
+++ webpack.config.js
@@ -26,10 +26,6 @@ const config = {
   resolve: {
     extensions: ['', '.js', '.jsx'],
   },
-  plugins: [
-    new DotenvPlugin({
-        path: './.env'
-     }),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
