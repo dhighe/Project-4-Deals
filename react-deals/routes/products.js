abb75326a27b2ef8802026a952b59996aad2229a
Donald Highe <dhighe25@gmail.com>
2016-12-14 18:22:28 -0500

    heroku is still being heroku...

--- react-deals/routes/products.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { findProducts } = require('../services/ebay.js');
-
-router.get('/', findProducts, (req, res) => {
-    res.json(res.products);
-  });
-
-module.exports = router;
