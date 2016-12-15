994fc608ca4fe1ce4b794e36d7b22e3b8dd3a051
Donald Highe <dhighe25@gmail.com>
2016-12-15 11:32:36 -0500

    Testing something

--- src/components/Head/Head.jsx
+++ src/components/Head/Head.jsx
@@ -5,7 +5,7 @@ import style from './Head.css';
 
 export default function Head(props){
   const handleSubmit = () => {
-    // event.preventDefault();
+    event.preventDefault();
 
   this.props.signUpUser(
     event.target.f_name.value,
